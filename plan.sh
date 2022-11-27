f7d3da9282fd7982230717dd59160baf2e997fc4
rahmedzaizi <rahmed@zaizi.com>
2022-11-27 00:37:58 +0000

    fixed for_each mapping error

--- plan.sh
+++ plan.sh
@@ -10,6 +10,7 @@ function cleanup {
 
 trap cleanup EXIT
 
+cd platform
 terraform -v
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars 2> error.txt
