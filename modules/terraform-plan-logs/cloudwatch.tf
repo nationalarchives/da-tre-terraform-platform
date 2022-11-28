d50ff28d3e62dfd2dc7ffa56edc18afbd4a3cb13
rahmedzaizi <rahmed@zaizi.com>
2022-11-28 18:53:12 +0000

    log group for terraform plan

--- modules/terraform-plan-logs/cloudwatch.tf
+++ modules/terraform-plan-logs/cloudwatch.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "tre-terraform-plan" {
   for_each = toset([ "dev", "test", "int", "staging", "prod", "tf-backend"  ])
-  name = "${var.prefix}-terraform-plan-${each.key}"
+  name = "${var.prefix}-${each.key}-terraform-plan"
 }
 
 resource "aws_cloudwatch_log_group" "tre_github_actions_error" {
