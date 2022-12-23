9c7d652cc9b7d75a947c8b17dfac29d3bed27f18
dg-zaizi <99955341+dg-zaizi@users.noreply.github.com>
2022-12-23 13:11:11 +0000

    Add module for codeartifact

--- modules/codeartifact/main.tf
+++ modules/codeartifact/main.tf
@@ -6,13 +6,13 @@ resource "aws_codeartifact_domain" "tre_v2" {
 # Define the upstream PyPi repository for public artifacts
 resource "aws_codeartifact_repository" "tre_v2_upstream" {
   repository = "pypi-store"
-  domain = aws_codeartifact_domain.tre_v2
+  domain     = aws_codeartifact_domain.tre_v2
 }
 
 # Define the TRE v2 CodeArtifact repository
 resource "aws_codeartifact_repository" "tre_v2" {
   repository = "tre-ca-artifacts"
-  domain = aws_codeartifact_domain.tre_v2
+  domain     = aws_codeartifact_domain.tre_v2
 
   upstream {
     repository_name = aws_codeartifact_repository.tre_v2_upstream.repository
