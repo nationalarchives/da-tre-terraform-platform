ecf6241856436d1f19ced0ddce32c9dda846f8cf
rahmedzaizi <rahmed@zaizi.com>
2023-01-23 16:44:02 +0000

    roles assumption changed for prod and nonprod terraform-role

--- modules/terraform-roles/variables.tf
+++ modules/terraform-roles/variables.tf
@@ -5,12 +5,12 @@ variable "prefix" {
 
 variable "roles_can_assume_terraform_role" {
   description = "ARNs of the OpenID Connect Roles in the management account that can asssume terraform role"
-  type        = string
+  type        = list(string)
 }
 
 variable "roles_can_assume_terraform_backend_role" {
   description = "ARNs of the OpenID Connect Platform Role in the management account that can asssume terraform backend role"
-  type        = string
+  type        = list(string)
 }
 
 
