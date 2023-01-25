89b1167065cb39704967a1db527ae0e944471e91
rahmedzaizi <rahmed@zaizi.com>
2023-01-25 15:01:10 +0000

    removed tf modules for tre-users account

--- tf-backend/data.tf
+++ tf-backend/data.tf
@@ -6,14 +6,6 @@ data "aws_region" "management" {
   provider = aws
 }
 
-data "aws_caller_identity" "users" {
-  provider = aws.users
-}
-
-data "aws_region" "users" {
-  provider = aws.users
-}
-
 data "aws_caller_identity" "nonprod" {
   provider = aws.nonprod
 }
