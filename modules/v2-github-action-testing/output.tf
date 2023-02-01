5260284369ac7cd37e79e038f8e86bf77e94d392
rahmedzaizi <rahmed@zaizi.com>
2023-02-01 16:03:40 +0000

    testing roles/policies added to tf_backned managed list

--- modules/v2-github-action-testing/output.tf
+++ modules/v2-github-action-testing/output.tf
@@ -2,3 +2,8 @@ output "tre_v2_github_action_testing_role_arn" {
   value = aws_iam_role.v2_github_action_testing.arn
   description = "ARN of the v2 GitHub Action Testing Role"
 }
+
+output "tre_v2_github_action_testing_policy_arn" {
+  value = aws_iam_policy.v2_github_action_testing.arn
+  description = "ARN of the v2 GitHub Action Testing Role"
+}
