be7fd029e41477334b59ce1f8352ec9a7d8dda5f
rahmedzaizi <rahmed@zaizi.com>
2023-03-22 08:35:18 +0000

    adding cw_log group for pte

--- modules/terraform-plan-logs/cloudwatch.tf
+++ modules/terraform-plan-logs/cloudwatch.tf
@@ -1,8 +1,5 @@
-resource "aws_cloudwatch_log_group" "tre-terraform-plan" {
-  for_each = toset(["dev", "test", "int", "staging", "prod", "tf-backend", "platform"])
-  name     = "${var.prefix}-${each.key}-terraform-plan"
+resource "aws_cloudwatch_log_group" "tre-github-actions-logs" {
+  for_each = toset(["dev", "test", "int", "staging", "prod", "tf-backend", "platform", "pte"])
+  name     = "${var.prefix}-${each.key}-github-actions-logs"
 }
 
-resource "aws_cloudwatch_log_group" "tre_github_actions_error" {
-  name = "${var.prefix}-github-actions-error"
-}
