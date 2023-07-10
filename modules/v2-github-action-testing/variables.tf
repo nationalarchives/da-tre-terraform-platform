358a02835c659db5f2e04d4cba2455c6f33a2bc3
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2023-07-10 12:47:08 +0100

    Fix: Declare new variables being used

--- modules/v2-github-action-testing/variables.tf
+++ modules/v2-github-action-testing/variables.tf
@@ -3,6 +3,16 @@ variable "prefix" {
   type        = string
 }
 
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
 variable "roles_can_assume_v2_github_action_testing_role" {
   description = "ARNs of the OpenID Connect Roles in the management account that can asssume v2-testing role"
   type        = list(string)
