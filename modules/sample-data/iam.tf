0ff19920b13c28c716c5048726e3ffbd4bf423b0
Mark Kingsbury <mark@kingsburydata.com>
2023-09-13 15:39:38 +0100

    DTE-853: kms for sample data bucket

--- modules/sample-data/iam.tf
+++ modules/sample-data/iam.tf
@@ -16,3 +16,35 @@ data "aws_iam_policy_document" "da_transform_sample_data_bucket" {
     resources = ["${aws_s3_bucket.da_transform_sample_data.arn}/*", aws_s3_bucket.da_transform_sample_data.arn]
   }
 }
+
+data "aws_iam_policy_document" "da_transform_sample_data_bucket_kms" {
+
+  statement {
+    sid     = "Allow access for Key Administrators"
+    actions = ["kms:*"]
+    effect  = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id_mgmnt}:root"]
+    }
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow sample data readers key"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [
+        var.account_id_prod,
+        var.account_id_nonprod
+      ]
+    }
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+  }
+}
