cf8d5a0f07069693536fd2d3f9c2d112d4d44739
Mark Kingsbury <mark@kingsburydata.com>
2023-11-01 11:07:19 +0000

    DTE-855: disable versioning in sample data bucket

--- modules/sample-data/s3.tf
+++ modules/sample-data/s3.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "da_transform_samp
 resource "aws_s3_bucket_versioning" "da_transform_sample_data" {
   bucket = aws_s3_bucket.da_transform_sample_data.id
   versioning_configuration {
-    status = "Enabled"
+    status = "Disabled"
   }
 }
 
