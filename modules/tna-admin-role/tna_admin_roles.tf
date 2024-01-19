f8a573e61fadafd68a6948746b6d7061ca081ca6
Annie Hawes <annie.hawes@nationalarchives.gov.uk>
2024-01-19 13:17:23 +0000

    TDR 3682- revert admin role terraform (#86)
    * Revert "Put admin user role under backend management in each aws account (#85)"
    
    This reverts commit feeedcaeea6be4b2d50b92cfa15d937ac2586e70.
    
    * Revert "TDR 3682 - Enable cross account access for running of security reports (#84)"
    
    This reverts commit 6e8d137228f9cdcb4b06e05c03881df24eda8bc3.

--- modules/tna-admin-role/tna_admin_roles.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_role" "tna_admin_role" {
-  name               = var.name
-  assume_role_policy = data.aws_iam_policy_document.admin_role_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "attach_managed_admin_policy" {
-  role       = aws_iam_role.tna_admin_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
